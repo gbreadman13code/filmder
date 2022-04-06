5575992e25f711a76493f863b5d148e4e08588cf
gbreadman13code <gbreadman13code@gmail.com>
2022-04-06 15:26:31 +0700

    d

--- private/private.js
+++ private/private.js
@@ -1,4 +1,4 @@
-export const API_KEY = 'f69b118dd3400a0d7186c71e11522057'
+export const API_KEY = 'fsdfdsfdsfdsfdsf'
 
 export const AUTH_URL = `https://api.themoviedb.org/3/authentication/token/new?api_key=${API_KEY}`
 
