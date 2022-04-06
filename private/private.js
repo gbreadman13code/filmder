fb577c9954ad4232b5799953f503af6f7cea43de
gbreadman13code <52678024+gbreadman13code@users.noreply.github.com>
2022-04-06 14:17:59 +0700

    Add files via upload

---
+++ private/private.js
@@ -0,0 +1,7 @@
+export const API_KEY = 'f69b118dd3400a0d7186c71e11522057'
+
+export const AUTH_URL = `https://api.themoviedb.org/3/authentication/token/new?api_key=${API_KEY}`
+
+export const SESSION_URL = `https://api.themoviedb.org/3/authentication/session/new?api_key=${API_KEY}`
+
+
