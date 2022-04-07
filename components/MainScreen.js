6c784074640eadcf0911bfef5831a1c7e613f65d
gbreadman13code <gbreadman13code@gmail.com>
2022-04-07 22:07:32 +0700

    ааа

--- components/MainScreen.js
+++ components/MainScreen.js
@@ -1,10 +1,16 @@
 import { StyleSheet, Text, Image, View, ScrollView, TouchableOpacity, ActivityIndicator, ImageBackground } from 'react-native'
 import React, { useState, useEffect } from 'react'
-import { API_KEY } from '../private/private'  
-import GifImage from '@lowkey/react-native-gif';
+import { API_KEY } from '../private/private'
+import { useFonts } from 'expo-font';
+
 
 
 const MainScreen = ({ navigation }) => {
+
+
+  let [fontsLoaded] = useFonts({
+    'Source-Sans-Pro': require('../assets/SourceSansPro-Regular.ttf'),
+  });
 
   const [randomMovie, setRandomMovie] = useState({
     id: '',
@@ -16,11 +22,65 @@ const MainScreen = ({ navigation }) => {
     genre: [],
     duration: ''
   })
-  const [likedMovies, setLikedMovies] = useState([
-    
-  ])
+  const [likedMovies, setLikedMovies] = useState([])
   const [isLoad, setIsLoad] = useState(false)
   const [likeAnimation, setLikeAnimation] = useState(false)
+  const [arrayOfFilms, setArrayOfFilms] = useState([
+    { id: '', name: '', description: '', image: '', release: '', isAdult: false, genre: [], duration: '' },
+    { id: '', name: '', description: '', image: '', release: '', isAdult: false, genre: [], duration: '' }
+  ])
+
+  const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+  }
+  const makeRuntime = (time) => {
+    let hours = Math.floor(time / 60)
+    let minutes = time - hours * 60
+    if (hours !== 0) {
+      return `${hours} ч. ${minutes} м.`
+    } else {
+      return `${minutes} м.`
+    }
+  }
+
+  const loadTwentyFilms = () => {
+    while (arrayOfFilms.length < 20) {
+
+        const randMovieId = getRandomInt(1, 999999)
+        fetch(`https://api.themoviedb.org/3/movie/${randMovieId}?api_key=${API_KEY}&language=ru`)
+          .then(responsive => responsive.json())
+          .then(result => {
+            if (result.status_code == 34) {
+              nextFilmHandler()
+            } else {
+              if (result.poster_path == null || result.release_date == '' || result.genres == [] || result.title.charCodeAt() < 1040 || result.title.charCodeAt() > 1103) {
+                nextFilmHandler()
+              } else {
+                
+                setRandomMovie({
+                  id: result.id,
+                  name: result.title,
+                  description: result.overview,
+                  image: result.poster_path,
+                  release: result.release_date.slice(0, -6),
+                  isAdult: result.adult,
+                  genre: result.genres.map(item => `${item.name}`),
+                  duration: makeRuntime(result.runtime)
+                })
+                setIsLoad(false)
+              }
+            }
+            setArrayOfFilms(arrayOfFilms => [...arrayOfFilms, {id: result.id, name: result.title, description: result.overview, image: result.poster_path, release: result.release_date.slice(0, -6), isAdult: result.adult, genre: result.genres.map(item => `${item.name}`), duration: makeRuntime(result.runtime)}])
+          })
+      
+    }
+  }
+  loadTwentyFilms()
+  console.log(arrayOfFilms)
+
+
 
   const addToLoved = () => {
     const isLiked = likedMovies.some(item => item.id === randomMovie.id)
@@ -31,23 +91,15 @@ const MainScreen = ({ navigation }) => {
         nextFilmHandler()
       }, 900)
       setLikedMovies(likedMovies => [...likedMovies, randomMovie])
-      
-    } else {
-      console.log('This film is liked early')
     }
   }
 
   const moveToWishList = () => {
-    navigation.navigate('Избранное', {likedMovies})
+    navigation.navigate('Избранное', { likedMovies })
   }
 
   const nextFilmHandler = () => {
     setIsLoad(true)
-    const getRandomInt = (min, max) => {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
-    }
     const randMovieId = getRandomInt(1, 999999)
     fetch(`https://api.themoviedb.org/3/movie/${randMovieId}?api_key=${API_KEY}&language=ru`)
       .then(responsive => responsive.json())
@@ -58,15 +110,7 @@ const MainScreen = ({ navigation }) => {
           if (result.poster_path == null || result.release_date == '' || result.genres == [] || result.title.charCodeAt() < 1040 || result.title.charCodeAt() > 1103) {
             nextFilmHandler()
           } else {
-            const makeRuntime = (time) => {
-              let hours = Math.floor(time/60)
-              let minutes = time - hours*60
-              if (hours !== 0) {
-                return `${hours} ч. ${minutes} м.`
-              } else {
-                return `${minutes} м.`
-              }
-            }
+            
             setRandomMovie({
               id: result.id,
               name: result.title,
@@ -90,16 +134,16 @@ const MainScreen = ({ navigation }) => {
     <>
 
       <View style={{ flex: 15, justifyContent: 'center', alignItems: 'center', backgroundColor: '#000' }}>
-      {likeAnimation === true ? <Image 
-        style={styles.addLike}
-        source={{
-          width: 400,
-          height: 400,
-          uri: 'https://data.whicdn.com/images/266332223/original.gif'
-        }}
-      /> : <></>}
-      
-      
+        {likeAnimation === true ? <Image
+          style={styles.addLike}
+          source={{
+            width: 400,
+            height: 400,
+            uri: 'https://data.whicdn.com/images/266332223/original.gif'
+          }}
+        /> : <></>}
+
+
         {isLoad === true ?
           <>
             <ActivityIndicator size='large' color='white' />
@@ -107,13 +151,13 @@ const MainScreen = ({ navigation }) => {
           </>
           :
           <>
-           <TouchableOpacity style={styles.navButtonLeft} onPress={addToLoved} onLongPress={moveToWishList}></TouchableOpacity>
-        <TouchableOpacity style={styles.navButtonRight} activeOpacity={0.1} onPress={nextFilmHandler}></TouchableOpacity>
-            <ScrollView style={{width: '100%',}}>
+            <TouchableOpacity style={styles.navButtonLeft} onPress={addToLoved} onLongPress={moveToWishList}></TouchableOpacity>
+            <TouchableOpacity style={styles.navButtonRight} activeOpacity={0.1} onPress={nextFilmHandler}></TouchableOpacity>
+            <ScrollView style={{ width: '100%', }}>
               <ImageBackground source={{ uri: `https://www.themoviedb.org/t/p/w1280/${randomMovie.image}` }} style={styles.backgroundImage} blurRadius={90}>
                 <View style={styles.imageContainer}>
                   {randomMovie.isAdult == true ? <Image style={styles.disclaimer} source={{ width: 90, height: 90, uri: 'https://cdn-icons-png.flaticon.com/512/3728/3728706.png' }} /> : <View></View>}
-                  <Image style={{borderRadius: 30}} source={{ width: 266, height: 400, uri: `https://www.themoviedb.org/t/p/w1280/${randomMovie.image}` }} />
+                  <Image style={{ borderRadius: 30 }} source={{ width: 266, height: 400, uri: `https://www.themoviedb.org/t/p/w1280/${randomMovie.image}` }} />
                 </View>
               </ImageBackground>
               <View style={{ backgroundColor: 'black', flex: 2 }}>
@@ -159,7 +203,6 @@ const styles = StyleSheet.create({
   },
   backgroundImage: {
     width: '100%',
-    // height: '100%',
   },
   addLike: {
     position: 'absolute',
@@ -187,15 +230,6 @@ const styles = StyleSheet.create({
     right: 0,
     zIndex: 2,
   },
-  textHeader: {
-    color: '#fff',
-    fontSize: 25,
-    width: '100%',
-    display: 'flex',
-    flexWrap: 'wrap',
-    textTransform: 'uppercase',
-    marginVertical: 10,
-  },
   genres: {
     paddingVertical: 5,
     paddingHorizontal: 10,
@@ -209,12 +243,13 @@ const styles = StyleSheet.create({
     marginTop: 5
   },
   descriptionWrapper: {
-    backgroundColor: '#000',
-    paddingHorizontal: 40,
-    width: '100%'
+    paddingHorizontal: 20,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15
   },
   description: {
     color: '#fff',
-    fontSize: 17
+    fontSize: 17,
+    fontFamily: 'Source-Sans-Pro'
   },
 })
